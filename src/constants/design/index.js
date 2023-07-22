2e20687bfa39a3e1d9b5e7b725d14d043cc843ea
aiden0407 <cailyent0407@gmail.com>
2023-07-23 01:22:56 +0900

    [Add] 디자인 상수 추가

---
+++ src/constants/design/index.js
@@ -0,0 +1,52 @@
+export const COLOR = {
+    N200: '#FAFAFA',
+    N300: '#F5F5F5',
+    N400: '#F0F0F0',
+    N500: '#D9D9D9',
+    N600: '#BFBFBF',
+    N700: '#8C8C8C',
+    N800: '#595959',
+    N900: '#434343',
+    N950: '#333333',
+    N1000: '#262626',
+    N1100: '#1F1F1F',
+    N1200: '#141414',
+    BLUE1: '#2081E2',
+    BLUE2: '#6DB6FF',
+    RED1: '#F36B29',
+    GREEN1: '#15AF4A',
+    GREEN2: '#26CA5E',
+}
+
+export const TYPOGRAPHY = {
+    H1: {
+        SIZE: '38px',
+    },
+    H2: {
+        SIZE: '30px',
+    },
+    H3: {
+        SIZE: '24px',
+    },
+    H4: {
+        SIZE: '20px',
+    },
+    H5: {
+        SIZE: '16px',
+    },
+    B0: {
+        SIZE: '15px',
+    },
+    B1: {
+        SIZE: '14px',
+    },
+    B2: {
+        SIZE: '13px',
+    },
+    B3: {
+        SIZE: '12px',
+    },
+    B4: {
+        SIZE: '10px',
+    },
+}
