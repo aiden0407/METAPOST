af22a039bd902479596586b989805cc3d2b271ac
aiden0407 <cailyent0407@gmail.com>
2023-07-22 17:30:24 +0900

    [Temp] App.js 초기설정

--- src/App.js
+++ src/App.js
@@ -3,25 +3,13 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 //Pages
-import Landing from './pages/Landing';
-import Login from './pages/Login';
-import Join from './pages/Join';
-import Home from './pages/Home';
-import Workout from './pages/Workout';
-import Food from './pages/Food';
-import Feedback from './pages/Feedback';
+import Login from 'pages/Login';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/join" element={<Join />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/workout" element={<Workout />} />
-        <Route path="/food" element={<Food />} />
-        <Route path="/feedback" element={<Feedback />} />
+        <Route path="/" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
