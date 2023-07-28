58abf53c9f0ce861ed1910de366e5a6a3046b09e
aiden0407 <cailyent0407@gmail.com>
2023-07-28 20:39:40 +0900

    [Update] appContext에 신고 데이터 추가

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -4,6 +4,10 @@ import { createContext, useReducer } from "react";
 //initial state
 const initialState = {
   isReportPopupOpened: false,
+  reportData: {
+    subject: undefined,
+    id: undefined
+  }
 };
 
 //create context
@@ -12,18 +16,26 @@ const AppContext = createContext({});
 //create reducer
 const reducer = (state, action) => {
   switch (action.type) {
-    
+
     case 'OPEN_REPORT_POPUP':
       return {
         ...state,
         isReportPopupOpened: true,
+        reportData: {
+          subject: action.subject,
+          id: action.id
+        }
       };
 
-      case 'CLOSE_REPORT_POPUP':
-        return {
-          ...state,
-          isReportPopupOpened: false,
-        };
+    case 'CLOSE_REPORT_POPUP':
+      return {
+        ...state,
+        isReportPopupOpened: false,
+        reportData: {
+          subject: undefined,
+          id: undefined
+        }
+      };
 
     default:
       return state;
