bca704e96f25799af0764a6315f13af0bc10f321
aiden0407 <cailyent0407@gmail.com>
2023-07-30 00:14:58 +0900

    [Update] 커뮤니티 api 추가

--- src/apis/Community/index.js
+++ src/apis/Community/index.js
@@ -1,7 +1,7 @@
 //API
 import axios from 'axios';
 
-export const getCommunityList = async function (accessToken) {
+export const getMyCommunityList = async function (accessToken) {
   try {
     let options = {
       url: `${process.env.REACT_APP_API_HOST}/community/user`,
@@ -9,6 +9,152 @@ export const getCommunityList = async function (accessToken) {
       headers: {
           'Authorization': `Bearer ${accessToken}`,
       },
+    }
+    const response = await axios(options);
+    return response;
+
+  } catch (error) {
+    throw error.response.data;
+  }
+}
+
+export const getCommunityRanking = async function (offset) {
+  try {
+    let options = {
+      url: `${process.env.REACT_APP_API_HOST}/community/ranking`,
+      method: 'POST',
+      data: {
+        offset: offset,
+      }
+    }
+    const response = await axios(options);
+    return response;
+
+  } catch (error) {
+    throw error.response.data;
+  }
+}
+
+export const getCommunityInfo = async function (accessToken, communityId, type, offset) {
+  try {
+    let options = {
+      url: `${process.env.REACT_APP_API_HOST}/community/info`,
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      data: {
+        community_id: communityId,
+        tap_type: type,
+        offset: offset,
+      }
+    }
+    const response = await axios(options);
+    return response;
+
+  } catch (error) {
+    throw error.response.data;
+  }
+}
+
+export const getCommunityDetail = async function (accessToken, communityId) {
+  try {
+    let options = {
+      url: `${process.env.REACT_APP_API_HOST}/community/detail`,
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      data: {
+        community_id: communityId,
+      }
+    }
+    const response = await axios(options);
+    return response;
+
+  } catch (error) {
+    throw error.response.data;
+  }
+}
+
+export const postCommunityJoin = async function (accessToken, communityId) {
+  try {
+    let options = {
+      url: `${process.env.REACT_APP_API_HOST}/community/join`,
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      data: {
+        community_id: communityId,
+      }
+    }
+    const response = await axios(options);
+    return response;
+
+  } catch (error) {
+    throw error.response.data;
+  }
+}
+
+export const postCommunityVerification = async function (accessToken, communityId) {
+  try {
+    let options = {
+      url: `${process.env.REACT_APP_API_HOST}/community/verify`,
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      data: {
+        community_id: communityId,
+      }
+    }
+    const response = await axios(options);
+    return response;
+
+  } catch (error) {
+    throw error.response.data;
+  }
+}
+
+export const createCommunity = async function (accessToken, name, description, logo, banner) {
+  try {
+    let options = {
+      url: `${process.env.REACT_APP_API_HOST}/community/create`,
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      data: {
+        title: name,
+        description: description,
+        logo_url: logo,
+        banner_url: banner,
+      }
+    }
+    const response = await axios(options);
+    return response;
+
+  } catch (error) {
+    throw error.response.data;
+  }
+}
+
+export const editCommunity = async function (accessToken, communityId, name, description, logo, banner) {
+  try {
+    let options = {
+      url: `${process.env.REACT_APP_API_HOST}/community/edit`,
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      data: {
+        community_id: communityId,
+        title: name,
+        description: description,
+        logo_url: logo,
+        banner_url: banner,
+      }
     }
     const response = await axios(options);
     return response;
