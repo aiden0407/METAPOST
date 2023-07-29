8fca2a2332bb6fbf5ef48fe502c413edbba5396c
aiden0407 <cailyent0407@gmail.com>
2023-07-30 00:17:15 +0900

    [Update] 검색 컴포넌트 로직 수정

--- src/components/Popup/Search/index.js
+++ src/components/Popup/Search/index.js
@@ -1,13 +1,14 @@
 //React
 import { useState, useContext } from 'react';
 import { AppContext } from 'context/AppContext';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 //Components
 import { COLOR } from 'constants/design';
 import { Text } from 'components/Text';
 import { Image } from 'components/Image';
-import { Row, FlexBox } from 'components/Flex';
+import { Row, Column } from 'components/Flex';
 
 //Api
 import { searchContents } from 'apis/Home';
@@ -16,14 +17,17 @@ import { searchContents } from 'apis/Home';
 import arrowBackIcon from 'assets/icons/arrow_back.svg';
 import fireInactiveIcon from 'assets/icons/fire_inactive.svg';
 import fireColorIcon from 'assets/icons/fire_color.svg';
+import nftIcon from 'assets/icons/icon_nft.png';
+import defaultProfile from 'assets/icons/icon_default_profile.png';
 
 function Search() {
 
+  const navigate = useNavigate();
   const { dispatch } = useContext(AppContext);
   const [searchText, setSearchText] = useState('');
   const [isToggleOpened, setIsToggleOpened] = useState(false);
   const [selectedOption, setSelectedOption] = useState('community');
-  const [searchResult, setSearchResult] = useState();
+  const [searchResult, setSearchResult] = useState([]);
 
   function handleSearchClose() {
     dispatch({ type: 'CLOSE_SEARCH_POPUP' });
@@ -37,7 +41,8 @@ function Search() {
   };
 
   function handleSelectedOption(type) {
-    setSelectedOption(type);
+    setSelectedOption();
+    setSearchResult([]);
     if (searchText.length) {
       handleSearch(searchText, type);
     }
@@ -47,10 +52,15 @@ function Search() {
     try {
       const response = await searchContents(text, type);
       setSearchResult(response.data);
+      setSelectedOption(type);
     } catch (error) {
       alert(error);
     }
   };
+
+  const handleImageError = (error) => {
+    error.target.src = defaultProfile;
+  }
 
   return (
     <PopupContainer>
@@ -69,44 +79,106 @@ function Search() {
       {
         isToggleOpened && <SearchTapWrapper>
           <SearchTap>
-            <StyledRow
+            <TapRow
               onClick={() => { handleSelectedOption('community') }}
               selected={selectedOption === 'community'}
             >
               <Image src={selectedOption === 'community' ? fireColorIcon : fireInactiveIcon} width={16} />
               <Text B1 color={selectedOption === 'community' ? COLOR.N800 : COLOR.N600} marginTop={1} marginLeft={4}>Community</Text>
-            </StyledRow>
-            <StyledRow
+            </TapRow>
+            <TapRow
               onClick={() => handleSelectedOption('post')}
               selected={selectedOption === 'post'}
             >
               <Image src={selectedOption === 'post' ? fireColorIcon : fireInactiveIcon} width={16} />
               <Text B1 color={selectedOption === 'post' ? COLOR.N800 : COLOR.N600} marginTop={1} marginLeft={4}>Post</Text>
-            </StyledRow>
-            <StyledRow
+            </TapRow>
+            <TapRow
               onClick={() => handleSelectedOption('user')}
               selected={selectedOption === 'user'}
             >
               <Image src={selectedOption === 'user' ? fireColorIcon : fireInactiveIcon} width={16} />
               <Text B1 color={selectedOption === 'user' ? COLOR.N800 : COLOR.N600} marginTop={1} marginLeft={4}>User</Text>
-            </StyledRow>
+            </TapRow>
           </SearchTap>
         </SearchTapWrapper>
       }
       {
         searchText
           ? <ResultWrapper>
-            <ResultColumn>
-
-
-
-
-
-
-
-
-              
-            </ResultColumn>
+            {
+              searchResult?.length
+                ? <>
+                  {
+                    selectedOption === 'community' && <>
+                      <ResultColumnCommunity>
+                        {
+                          searchResult.map((item) =>
+                            <StyledRow
+                              key={`community_${item.pk}`}
+                              onClick={() => {
+                                navigate(`/community?community_id=${item.pk}`);
+                                window.scrollTo({ top: 0 });
+                                handleSearchClose();
+                              }}
+                            >
+                              <Image src={item.fields.logo_url} width={33} height={33} borderRadius="4px" onError={handleImageError} />
+                              <Text B0 color={COLOR.N800} marginLeft={8}>{item.fields.title}</Text>
+                            </StyledRow>
+                          )
+                        }
+                      </ResultColumnCommunity>
+                    </>
+                  }
+                  {
+                    selectedOption === 'post' && <>
+                      <ResultColumnPost>
+                        {
+                          searchResult.map((item) =>
+                            <StyledColumn
+                              key={`post_${item.pk}`}
+                              gap={4}
+                              onClick={() => {
+                                navigate(`/post?post_id=${item.pk}`);
+                                window.scrollTo({ top: 0 });
+                                handleSearchClose();
+                              }}
+                            >
+                              <Text B0 color={COLOR.N800}>{item.fields.title}</Text>
+                              <Text B0 color={COLOR.N600}>{item.fields.description}</Text>
+                            </StyledColumn>
+                          )
+                        }
+                      </ResultColumnPost>
+                    </>
+                  }
+                  {
+                    selectedOption === 'user' && <>
+                      <ResultColumnUser>
+                        {
+                          searchResult.map((item) =>
+                            <StyledRow key={`comment_${item.pk}`}>
+                              <Image src={item.fields?.nft_thumbnail ?? defaultProfile} width={33} height={33} borderRadius="4px" />
+                              <Column marginLeft={8} gap={4}>
+                                <Text B2 color={COLOR.N600} style={{ lineHeight: '100%' }}>{item.fields.nickname}</Text>
+                                <Row>
+                                  <Image src={nftIcon} width={16} />
+                                  <Text B3 medium color={COLOR.N600} marginLeft={4}>{item.fields?.nft_title}</Text>
+                                </Row>
+                              </Column>
+                            </StyledRow>
+                          )
+                        }
+                      </ResultColumnUser>
+                    </>
+                  }
+                </>
+                : <NoResultArea>
+                  {
+                    selectedOption && <Text H5 color={COLOR.N700}>No Result</Text>
+                  }
+                </NoResultArea>
+            }
           </ResultWrapper>
           : <TouchableArea
             onClick={() => {
@@ -201,13 +273,43 @@ const ResultWrapper = styled.div`
   justify-content: center;
 `
 
-const ResultColumn = styled.div`
+const ResultColumnCommunity = styled.div`
   width: 100%;
   max-width: 640px;
   height: 100%;
-  padding: 11px 16px;
+  padding: 12px 16px;
   display: flex;
+  flex-direction: column;
   gap: 22px;
+`
+
+const ResultColumnPost = styled.div`
+  width: 100%;
+  max-width: 640px;
+  height: 100%;
+  padding: 8px 16px;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`
+
+const ResultColumnUser = styled.div`
+  width: 100%;
+  max-width: 640px;
+  height: 100%;
+  padding: 12px 16px;
+  display: flex;
+  flex-direction: column;
+  gap: 22px;
+`
+
+const NoResultArea = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: #FFFFFF;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
 
 const TouchableArea = styled.div`
@@ -216,10 +318,18 @@ const TouchableArea = styled.div`
   background-color: transparent;
 `
 
-const StyledRow = styled(Row)`
+const TapRow = styled(Row)`
   padding: 0 13px 0 11px;
-  border-bottom: ${(props) => props.selected && `2px solid ${COLOR.BLUE2}`};
+  border-bottom: ${(props) => props.selected ? `2px solid ${COLOR.BLUE2}` : `1px solid ${COLOR.N400}`};
   background-color: #FFFFFF;
+  cursor: pointer;
+`
+
+const StyledRow = styled(Row)`
+  cursor: pointer;
+`
+
+const StyledColumn = styled(Column)`
   cursor: pointer;
 `
 
