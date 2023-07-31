f87bbf7be9e7e7aff5f27c99cc89fd1be059f510
aiden0407 <cailyent0407@gmail.com>
2023-07-31 18:01:17 +0900

    [Update] 지갑 회원가입 로직 추가

--- src/apis/SignUp/index.js
+++ src/apis/SignUp/index.js
@@ -56,9 +56,27 @@ export const registerByEmail = async function (email, password, userName, wallet
         uid: email,
         password: password,
         nickname: userName,
-        wallet_adress: walletAdress,
+        wallet_address: walletAdress,
         nft_id: nftId
       }
+    }
+    const response = await axios(options);
+    return response;
+
+  } catch (error) {
+    throw error.response.data;
+  }
+}
+
+export const nftCheckByWallet = async function (walletAdress) {
+  try {
+    let options = {
+      url: `${process.env.REACT_APP_API_HOST}/get-nfts/${walletAdress}`,
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json",
+        "frontend-api-token": `0Iv6jCZBHf0ps0cmAOWyfeAehwniLFWe-9oybqHqEVxgaHP4JrGJV5ISirR0EhYQSS4i3o2jq-OQPFTrmv05XA`
+      },
     }
     const response = await axios(options);
     return response;
@@ -71,8 +89,13 @@ export const registerByEmail = async function (email, password, userName, wallet
 export const registerByWallet = async function (walletAdress, userName, nftId) {
   try {
     let options = {
-      url: `${process.env.REACT_APP_API_HOST}/?wallet_adress=${walletAdress}&user_name=${userName}&nft_id=${nftId}`,
-      method: 'GET',
+      url: `${process.env.REACT_APP_API_HOST}/wallet-sign-up`,
+      method: 'POST',
+      data: {
+        nickname: userName,
+        wallet_address: walletAdress,
+        nft_id: nftId
+      }
     }
     const response = await axios(options);
     return response;
