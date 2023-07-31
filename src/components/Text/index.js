0ae59bbb6e7df25fc52dbc01b92f294550dd505b
aiden0407 <cailyent0407@gmail.com>
2023-07-31 18:02:25 +0900

    [Design] AkiraExpanded 컴포넌트화

--- src/components/Text/index.js
+++ src/components/Text/index.js
@@ -8,7 +8,9 @@ function fontWeightSelector(bold, medium){
   return 'font-weight: 400; line-height: 150%;';
 }
 
-function fontTypeSelector(H1, H2, H3, H4, H5, B0, B1, B2, B3, B4){
+function fontTypeSelector(P1, P2, H1, H2, H3, H4, H5, B0, B1, B2, B3, B4){
+  if(P1) return `font-size: ${TYPOGRAPHY.P1.SIZE}; font-family: AkiraExpanded; font-weight: 800; line-height: 100%;`
+  if(P2) return `font-size: ${TYPOGRAPHY.P2.SIZE}; font-family: AkiraExpanded; font-weight: 800; line-height: 100%;`
   if(H1) return `font-size: ${TYPOGRAPHY.H1.SIZE};`
   if(H2) return `font-size: ${TYPOGRAPHY.H2.SIZE};`
   if(H3) return `font-size: ${TYPOGRAPHY.H3.SIZE};`
@@ -23,7 +25,7 @@ function fontTypeSelector(H1, H2, H3, H4, H5, B0, B1, B2, B3, B4){
 
 export const Text = styled.p`
   ${(props) => fontWeightSelector(props.bold, props.medium)}
-  ${(props) => fontTypeSelector(props.H1, props.H2, props.H3, props.H4, props.H5, props.B0, props.B1, props.B2, props.B3, props.B4)}
+  ${(props) => fontTypeSelector(props.P1, props.P2, props.H1, props.H2, props.H3, props.H4, props.H5, props.B0, props.B1, props.B2, props.B3, props.B4)}
   color: ${(props) => props.color ?? '#000000'};
   margin-top: ${(props) => `${props.marginTop}px`};
   margin-left: ${(props) => `${props.marginLeft}px`};
